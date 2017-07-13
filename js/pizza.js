8e9f7e3da844d3e58eab2f01e80a5003043b14c0
alvaro <amianalvarez@gmail.com>
2017-07-13 18:35:09 +0200

    pep & mushrooms

--- js/pizza.js
+++ js/pizza.js
@@ -1,1 +1,14 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function(){
+    $('.btn-pepperonni').click(ingredients);
+    $('.btn-mushrooms').click(ingredients);
+    $('.btn-green-pepper').click(ingredients);
+
+    function ingredients(){
+        if($(this).hasClass('btn-pepperonni')){
+            $('.pep').toggle('.pep');
+        }else if($(this).hasClass('btn-mushrooms')){
+           $('.mushroom').toggle('.mushroom');
+        }
+    }
+});
+
